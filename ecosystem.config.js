6c815e82d957b5158a226799d4ab59523d50281d
GBEZE Nutefe <martingbeze52@gmail.co>
2022-11-07 11:36:47 +0000

    first commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             exec_mode: 'cluster',
             instances: '1', // Ou un certain nombre d'instances
             script: './node_modules/nuxt/bin/nuxt.js',
-            cwd: '/var/www/nuxt/comptoir-agricole',
+            cwd: '/var/www/nuxt/comptoir-agricole-web',
             args: 'start'
         }
     ]
