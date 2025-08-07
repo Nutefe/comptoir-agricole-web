25c197cc94874d1efa85fa0cd61d52aa43f1da2d
cyberethik <contact@cyberethik.com>
2025-08-07 08:54:31 +0000

    conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    apps: [
-        {
-            name: 'Comptoiragricole',
-            exec_mode: 'cluster',
-            instances: '1', // Ou un certain nombre d'instances
-            script: './node_modules/nuxt/bin/nuxt.js',
-            cwd: '/var/www/nuxt/comptoir-agricole-web',
-            args: 'start'
-        }
-    ]
+  apps: [
+    {
+      name: 'Comptoiragricole',
+      exec_mode: 'cluster',
+      instances: '1', // Ou un certain nombre d'instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      cwd: '/var/www/html/comptoir-agricole-web',
+      args: 'start',
+    },
+  ],
 }
